04f6baed2ec3cbb9a5f6725e969e61f793fd9133
jilljurgens <jurgensjill@gmail.com>
2018-01-13 13:05:24 -0600

    more structure changes

---
+++ models/users.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  });
+
+ // Users.associate = function(models) {
+  //   // Associating Author with Posts
+  //   // When an Author is deleted, also delete any associated Posts
+  //   Users.hasMany(models.Post, {
+  //     onDelete: "cascade"
+  //   });
+  // };
+
+ return User;
+};
